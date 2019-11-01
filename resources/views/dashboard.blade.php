81ca4611d065f05e1148568429bc307d2e3a627a
NikosKarakatsidis <nikosxfx@hotmail.com>
2019-11-01 11:09:32 +0100

    xx

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
                         </div>
                     @endif
                     <a href="/posts/create" class = "btn btn-primary">Create Post</a>
-                        <h3>Your Blog Posts</h3>
+                        <h3>Your Blog Posts!</h3>
                         @if(count($posts)>0)
                         <table class="table table-striped">
                             <tr>
