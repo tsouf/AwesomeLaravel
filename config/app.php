7ef18a237882f05920c0b9dd853d0c4b484387c7
NikosKarakatsidis <nikosxfx@hotmail.com>
2019-11-01 10:37:31 +0100

    make it better

--- config/app.php
+++ config/app.php
@@ -163,8 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Unisharp\Ckeditor\ServiceProvider::class,   
-
+        Unisharp\Ckeditor\ServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -229,6 +228,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+      
     ],
 
 ];
