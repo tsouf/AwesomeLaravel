71673eeff08dea7a5fc43e8cc690d6fe29eb59b1
holales1 <alejandrovaltor.x2@gmail.com>
2019-11-06 11:21:20 +0100

    Laravel working on localhost

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -23,10 +23,10 @@
             </ul>
 
             <ul class="nav navbar-nav">
-              <li><a href="/">Home</a></li>
+              <li><a href="{{ url('/') }}/">Home</a></li>
               <li><a href="/about">About</a></li>
               <li><a href="/services">Services</a></li>
-              <li><a href="/posts">Blog</a></li>
+              <li><a href="{{ url('/') }}/posts">Blog</a></li>
             </ul>
 
             <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@
 });*/
 
 Route::get('/', 'PagesController@index' );
-Route::get('/about', 'PagesController@about' );
-Route::get('/services', 'PagesController@services' );
+
+
 
 //Automatically map routes to the functions
 Route::resource('posts','PostController');
