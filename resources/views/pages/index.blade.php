b9d30164f0b803521a0678c55de81a1327e308bf
NikosKarakatsidis <nikosxfx@hotmail.com>
2019-11-06 10:28:35 +0100

    test

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,7 +2,7 @@
 
   @section('content')
         <div class="jumbotron text-center">
-            <h1>Welcome!!!!</h1>
+           
         <p> This is the Laravel application which I created from scratch!!!"</p>
         <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a><a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
         </div>
