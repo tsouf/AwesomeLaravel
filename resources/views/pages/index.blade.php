44828f1fe1bc8b5852760b8d3f5fad4034a43dfd
NikosKarakatsidis <nikosxfx@hotmail.com>
2019-11-06 12:37:58 +0100

    ff

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,9 +1,8 @@
   @extends('layouts.app')
 
   @section('content')
-        <div class="jumbotron text-center">
-           
-        <p> This is the Laravel application which I created from scratch!!!"</p>
-        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a><a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
-        </div>
+
+  <img src="{{asset('/images/food.jpg')}}" class="center-fit" >
+
+        
 @endsection
