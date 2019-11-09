c0185b8c1459d82c4bb1d7b98edb52829987e134
holales1 <alejandrovaltor.x2@gmail.com>
2019-11-09 16:27:00 +0100

    Changed css in index Food

--- resources/views/food/index.blade.php
+++ resources/views/food/index.blade.php
@@ -4,15 +4,18 @@
 <h1>Posts</h1>
     @if(count($foods) > 0)
         @foreach($foods as $food)
+        <a href="{{ url('/') }}/foods/{{ $food->id }}">
         <div class="well row align-items-center">
-            <div class="col-sm-4 col-md-4 col-lg-4" >
-                <h3><a href="{{ url('/') }}/foods/{{ $food->id }}">{{ $food->title }}</a></h3>
-                <img id={{ $food->image }}  src="{{URL::asset('images/food.jpg')}}" width="100%">
+                
+            <div class="col-sm-12 col-md-4 col-lg-4" >
+                <h3 class="text-center">{{ $food->title }}</h3>
+                <img id={{ $food->image }}  src="{{URL::asset('images/food.jpg')}}" width="75%" style="margin-left: auto;margin-right: auto;display: block;">
             </div>
-            <div class="col-sm-8 col-md-8 col-lg-8" style="text-align: center; padding-top: 7%;" >
+            <div class="col-sm-12 col-md-8 col-lg-8" style="text-align: center; padding-top: 7%;" >
                 <p style="color: black;">{{ $food->description }}</p>
             </div>
         </div>
+    </a>
         @endforeach
         {{ $foods->links() }}
     @else 
