e62fce53e86f23b72613d196d77c88b081dbf1af
NikosKarakatsidis <nikosxfx@hotmail.com>
2019-11-11 18:30:03 +0100

    Further gui improvements

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,15 +3,56 @@
  
   @section('content')
 
-
   <img src="{{asset('/images/new1.jpg')}}" id="frontpic" >
 
   <br>
   <br>
+  <br>
+  <br>
+  <br>
+  
+   <div class="row3">
+     <section class="hoc container clear">
+       <div class="sectiontitle"  >
+         <h2 class="heading">
+           <i>About Us</i>
+          
+         </h2>
+         <br>
+         <br>
+         Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable, creative experience to be truly fulfilling.
+          Laravel attempts to take the pain out of development by easing common tasks used in the majority of web projects, such as authentication, routing, sessions, and caching.
+       </div>
+     </section>
+
+   </div>
+   <br>
+   <br>
+   <br>
+
+   <div class="row4">
+      <section class="hoc container clear">
+          <div class="sectiontitle"  >
+            <h2 class="heading">
+              <i>Contact Us</i>
+            </h2>
+            <br>
+            <br>
+            <strong> Email adrress:</strong>  nikosxfx@hotmail.com
+            <br>
+            <strong> Phone number:</strong>  +4571581194
+            <br>
+            <strong> Location:</strong> Esbjerg-Denmark
+            <br>
+            <strong> Postal code:</strong> 6705
+          </div>
+        </section>
+   </div>
+<br>
+<br>
    <div class="footer">
       <b>Copyright 2019. All rights reserved. </b>
    </div>
- 
 
 @endsection
 
